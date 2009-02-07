d32255992d7004ffb04729ba24fe35a72f0f4201
Craig Ambrose <craig@craigambrose.com>
2009-02-07 14:47:24 +1300

    removed pesky migration

--- db/migrate/20090206024400_create_auth_key_for_users_without_passwords.rb
+++ db/migrate/20090206024400_create_auth_key_for_users_without_passwords.rb
@@ -1,9 +1,5 @@
 class CreateAuthKeyForUsersWithoutPasswords < ActiveRecord::Migration
   def self.up
-    for user in User.find(:all, :conditions => {:password => nil})
-      user.send(:generate_auth_token)
-      user.save
-    end
   end
 
   def self.down
